156528f55314d2f6f3eacb6f27793744d1ab0dfe
Masiu <ciszekmarcell@gmail.com>
2020-04-16 19:00:37 +0200

    first commit

---
+++ src/Hello.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const Hello: React.FC<Props> = () => (
+  <div>
+    {' '}
+    <h1> Hello from a loaded component </h1>
+    {' '}
+  </div>
+);
+export default Hello;
