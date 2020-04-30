cd0d85488c435d33cb1e048b2d4ed7f048a1060a
Masiu <ciszekmarcell@gmail.com>
2020-04-30 09:36:50 +0200

    theme provide

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import './App.css';
 import Routes from './components/routes/Routes';
 import Layout from './components/layout/Layout';
+import ThemeProvider from './context/Theme';
 
 
 function App() {
   return (
     <>
-      <Layout>
-        <Routes />
-      </Layout>
+      <ThemeProvider>
+        <Layout>
+          <Routes />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/context/Theme.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable import/extensions */
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+
+interface ThemeProps{
+  children: React.ReactNode;
+}
+
+const { useReducer, createContext } = React;
+
+export interface InitialState {
+  isLight: boolean;
+  light: { hex: string; ui: string; bg: string };
+  dark: { hex: string; ui: string; bg: string };
+  setTheme: boolean;
+}
+
+export const initialState: InitialState = {
+  isLight: true,
+  light: { hex: '#333', ui: '#ddd', bg: '#eee' },
+  dark: { hex: '#ddd', ui: '#333', bg: '#555' },
+  setTheme: true,
+};
+
+export const ThemeContext = createContext<InitialState|Record<string, any>>(initialState);
+
+interface ToggleThemeAction {
+  type: 'TOGGLE_THEME';
+  payload: boolean;
+}
+
+
+type ActionTypes = ToggleThemeAction
+
+const themeReducer = (state: InitialState, action: ActionTypes) => {
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      return {
+        ...state,
+        setTheme: !state.setTheme,
+      };
+    default:
+      return state;
+  }
+};
+
+
+const ThemeProvider: React.FC<ThemeProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(themeReducer, initialState);
+  return (
+    <ThemeContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+export default ThemeProvider;
